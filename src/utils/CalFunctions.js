ead5a6804a62e1d7f9d7c6e79af36af934068027
MarshPeter <peter.m.farmer.204@gmail.com>
2024-10-16 14:52:42 +1100

    feature complete

--- src/utils/CalFunctions.js
+++ src/utils/CalFunctions.js
@@ -1,5 +1,17 @@
+const validNumbers = (numA, numB) => {
+  const validA = isNaN(numA);
+  const validB = isNaN(numB);
+
+  if (validA && validB) return "Both inputs need to be a valid number.";
+  if (validA) return "First input needs to be a valid number.";
+  if (validB) return "Second input needs to be a valid number.";
+
+  return "";
+}
+
 export const add = (numA, numB) => {
-  return numA + numB;
+  const areValidNumbers = validNumbers(numA, numB);
+  return areValidNumbers === "" ? numA + numB : areValidNumbers;
 };
 
 export const subtract = (numA, numB) => {
