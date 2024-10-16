15d53af28d51d944cf84d0b85199a455470c4d41
MarshPeter <peter.m.farmer.204@gmail.com>
2024-10-16 15:21:33 +1100

    test PR

--- src/utils/CalFunctions.js
+++ src/utils/CalFunctions.js
@@ -2,9 +2,9 @@ const validNumbers = (numA, numB) => {
   const validA = isNaN(numA);
   const validB = isNaN(numB);
 
-  if (validA && validB) return "Both inputs need to be a valid number.";
-  if (validA) return "First input needs to be a valid number.";
-  if (validB) return "Second input needs to be a valid number.";
+  if (validA && validB) return "Both inputs need to be a valid number";
+  if (validA) return "First input needs to be a valid number";
+  if (validB) return "Second input needs to be a valid number";
 
   return "";
 }
